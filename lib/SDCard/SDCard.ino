d35b1a89691fd9d3d1741ceb9d48cbaca7ab7b35
MarcMatten <marc@matten-online.de>
2020-06-12 17:46:20 +0200

    made SDCard.ino mode user-friendly and modular

--- lib/SDCard/SDCard.ino
+++ lib/SDCard/SDCard.ino
@@ -2,47 +2,52 @@
 #include <SD.h>
 
 const int chipSelect = 10;
-
+String defaultFileName = "default.csv";
+String defaultDataStr = "default header";
 File myFile;
 
-void setup() {
+void initSDCard(String fileName, String dataStr){
   if (!SD.begin(chipSelect)) {
     while (1);
   }
-
-  if (SD.exists("logfile.txt")) {
+  if (SD.exists(fileName)) {
     ;
-  } else {
-    myFile = SD.open("logfile.txt", FILE_WRITE);
-    myFile.println("der test");
-    myFile.close();
+  }
+  else {
+    writeToSDCard(fileName, dataStr);
   }
 }
 
-void loop() {
-  // make a string for assembling the data to log:
-  String dataString = "";
-
-  // read three sensors and append to the string:
-  for (int analogPin = 0; analogPin < 3; analogPin++) {
-    int sensor = analogRead(analogPin);
-    dataString += String(sensor);
-    if (analogPin < 2) {
-      dataString += ",";
-    }
-  }
-
+void writeToSDCard(String fileName, String dataStr){
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File dataFile = SD.open("logfile.txt", FILE_WRITE);
-
+  myFile = SD.open(fileName, FILE_WRITE);
   // if the file is available, write to it:
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
+  if (myFile){
+    myFile.println(dataStr);
+    myFile.close();
   }
   // if the file isn't open, pop up an error:
   else {
     ;
   }
+}
+
+void setup() {
+  initSDCard(defaultFileName, defaultDataStr);
+}
+
+void loop() {
+  // make a string for assembling the data to log:
+  String myDataString = "";
+
+  // read three sensors and append to the string:
+  for (int analogPin = 0; analogPin < 3; analogPin++) {
+    int sensor = analogRead(analogPin);
+    myDataString += String(sensor);
+    if (analogPin < 2) {
+      myDataString += ",";
+    }
+  }
+  writeToSDCard(defaultFileName, myDataString);
 }
